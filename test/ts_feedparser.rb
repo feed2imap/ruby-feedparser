475b8707920fb2d2795cd8d341176399be8bb2ea
lnu <lnu@1727c7e6-a704-0410-9340-b1ffd757133d>
2006-01-02 18:11:32 +0000

    added test suite
    git-svn-id: svn+ssh://svn.gna.org/svn/ruby-feedparser/trunk/ruby-feedparser@18 1727c7e6-a704-0410-9340-b1ffd757133d

---
+++ test/ts_feedparser.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby -w
+
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift File.join(File.dirname(__FILE__), '..', 'test')
+$:.unshift File.join(File.dirname(__FILE__), 'lib')
+$:.unshift File.join(File.dirname(__FILE__), 'test')
+
+require 'tc_feed_parse'
+require 'tc_htmloutput'
+require 'tc_parser'
+require 'tc_textoutput'
