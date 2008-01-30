ba91442c83a623db74aad2e7fa91d2ff58e95cf5
lnu <lnu@1727c7e6-a704-0410-9340-b1ffd757133d>
2008-01-30 09:10:38 +0000

    fixed to_human_readable
    git-svn-id: svn+ssh://svn.gna.org/svn/ruby-feedparser/trunk/ruby-feedparser@61 1727c7e6-a704-0410-9340-b1ffd757133d

--- lib/feedparser/filesizes.rb
+++ lib/feedparser/filesizes.rb
@@ -1,12 +1,14 @@
-class Fixnum
+class Integer
   def to_human_readable
     n = self
     if n < 1024
       return "#{n} B"
     elsif n >= 1024 and n < 1024*1024
       return "%.1f KB" % (n.to_f / 1024)
-    else
+    elsif n >= 1024*1024 and n < 1024*1024*1024
       return "%.1f MB" % (n.to_f / (1024*1024))
+    else
+      return "%.1f GB" % (n.to_f / (1024*1024*1024))
     end
   end
 end
